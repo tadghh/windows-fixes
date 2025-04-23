ce99480c0122a774d6a7b5795f5f80aa836135f8
�ethan <47073445+tadghh@users.noreply.github.com>
2025-04-23 12:07:15 -0500

    Create ssh-copy-id.ps1

---
+++ ssh-copy-id.ps1
@@ -0,0 +1,5 @@
+## Copy current users to host
+function ssh-copy-id([string]$sshHost)
+{
+    cat ~/.ssh/id_rsa.pub | ssh "$sshHost" "mkdir -p ~/.ssh && touch ~/.ssh/authorized_keys && chmod -R go= ~/.ssh && cat >> ~/.ssh/authorized_keys"
+}
